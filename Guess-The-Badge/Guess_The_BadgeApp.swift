4520aa44fbe7f646490ab832bb8199961b22a892
Reza Gharooni <reazgharooni@outlook.com>
2023-02-04 16:20:42 +0000

    Initial commit

---
+++ Guess-The-Badge/Guess_The_BadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_BadgeApp.swift
+//  Guess-The-Badge
+//
+//  Created by Reza Gharooni on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_BadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
