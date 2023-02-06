c2aaa77afd1d1ddcdc1220f01551db91f3f7ea26
Reza Gharooni <reazgharooni@outlook.com>
2023-02-06 19:55:45 +0000

    added badges and created game

--- Guess-The-Badge/ContentView.swift
+++ Guess-The-Badge/ContentView.swift
@@ -7,15 +7,75 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var score = 0
+    
+    @State private var footballTeams = ["Arsenal", "Aston Villa", "Bournemouth", "Chelsea", "Leicester", "Liverpool", "Manchester City", "Manchester United", "Newcastle", "Norwich", "Crystal Palace", "Southampton", "Sunderland", "Swansea", "Watford", "West Bromwich Albion", "West Ham"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color("dark green"), .white, Color("dark green")]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 20){
+                VStack {
+                    Text("Tap the badge of")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text(footballTeams[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                VStack(spacing:30) {
+                    ForEach(0..<3) { number in
+                        Button {
+                            badgeTapped(number)
+                        } label: {
+                            Image(footballTeams[number])
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .shadow(radius: 5)
+                            
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 20)
         }
-        .padding()
+        
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            if score == 1 {
+                Text("Your score is \(score) point")
+            } else {
+                Text("Your score is \(score) points")
+            }
+            
+        }
+    }
+    
+    func badgeTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Incorrect"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        footballTeams.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -24,3 +84,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
